468e0f6452e915cce78b32532851d0fc7cb8f4d1
Hselit <tileshkathiresan@gmail.com>
2025-05-09 16:39:13 +0530

    bootstrap added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-function login(){
-   alert("Login Success...");
+function login() {
+  alert("Login Success...");
+}
+console.log("hi form js");
+var myModal = document.getElementById("modell");
+
+console.log(myModal);
+var myInput = document.getElementById("viewbutton");
+console.log(myInput);
+
+myModal.addEventListener("click", function () {
+  //   myInput.focus();
+  console.log("hi");
+});
+
+function showmodel() {
+  console.log("jojojo");
+  const modalElement = document.getElementById("modell");
+  const modalInstance = new bootstrap.Modal(modalElement);
+  modalInstance.show();
 }
